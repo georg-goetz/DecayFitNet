708e8c0c844e54e52dbbdba731a6fd4cc3c79d9d
Georg Götz <georg.gotz@aalto.fi>
2022-01-20 18:32:34 +0200

    Make generate_input_transform fit for exactly_n_slope_mode

--- python/generate_input_transform.py
+++ python/generate_input_transform.py
@@ -1,9 +1,19 @@
 import toolbox.core as core
 import pickle
 
+N_SLOPES = 3
+EXACTLY_N_SLOPES_MODE = False
+
 if __name__ == '__main__':
     print('Reading dataset.')
-    dataset_decays = core.DecayDataset(n_slopes_max=3, edcs_per_slope=50000, testset_flag=False)
+    dataset_decays = core.DecayDataset(n_slopes_max=N_SLOPES, edcs_per_slope=50000, testset_flag=False,
+                                       exactly_n_slopes_mode=EXACTLY_N_SLOPES_MODE)
 
     input_transform = {"edcs_db_normfactor": dataset_decays.edcs_db_normfactor}
-    pickle.dump(input_transform, open('../model/input_transform.pkl', 'wb'))
+
+    if EXACTLY_N_SLOPES_MODE:
+        n_slopes_str = '_{}slopes'.format(N_SLOPES)
+    else:
+        n_slopes_str = ''
+
+    pickle.dump(input_transform, open('../model/input_transform{}.pkl'.format(n_slopes_str), 'wb'))
