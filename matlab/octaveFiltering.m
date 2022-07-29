8fcde7230e3978b9197d09fa980244e7931729ce
Georg Götz <georg.gotz@aalto.fi>
2022-07-29 19:09:14 +0300

    Decreased bandwidth of octave filters
    * Octave filters were too broad previously. More fine tuning might still be required.

--- matlab/octaveFiltering.m
+++ matlab/octaveFiltering.m
@@ -6,14 +6,14 @@ for bIdx = 1:numBands
     % Determine IIR filter coefficients for this band
     if fBands(bIdx) == 0
         % Lowpass band below lowest octave band
-        fCutoff = (1/sqrt(2))*fBands(bIdx+1);
+        fCutoff = (1/sqrt(1.5))*fBands(bIdx+1);
         [z, p, k] = butter(5, fCutoff/fs*2);
     elseif fBands(bIdx) == fs/2
         % Highpass band below lowest octave band
-        fCutoff = sqrt(2)*fBands(bIdx-1);
+        fCutoff = sqrt(1.5)*fBands(bIdx-1);
         [z, p, k] = butter(5, fCutoff/fs*2, 'high');
     else
-        thisBand = fBands(bIdx) .* [1/sqrt(2), sqrt(2)];
+        thisBand = fBands(bIdx) .* [1/sqrt(1.5), sqrt(1.5)];
         [z, p, k] = butter(5, thisBand/fs*2, 'bandpass');
     end
     
